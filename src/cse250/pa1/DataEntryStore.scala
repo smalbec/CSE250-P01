4b31087e2f2cc9671b4f0db27a260d1710a5926f
smalbec <44180894+smalbec@users.noreply.github.com>
2019-09-23 21:44:13 -0500

    stuck at deciding how to insert, maybe see into intera

--- src/cse250/pa1/DataEntryStore.scala
+++ src/cse250/pa1/DataEntryStore.scala
@@ -9,8 +9,8 @@
  * http://creativecommons.org/licenses/by-nc-sa/4.0/.
  *
  * Submission author
- * UBIT:
- * Person#:
+ * UBIT:smalbec
+ * Person#:50280232
  *
  * Collaborators (include UBIT name of each, comma separated):
  * UBIT:
@@ -21,25 +21,68 @@ import cse250.objects.EmbeddedListNode
 
 class DataEntryStore[A >: Null <: AnyRef](private val capacity: Int = 100)
   extends collection.mutable.Seq[A] {
+
+  //array of type emb list of capacity 100 populated by emb list
   private val dataArray = Array.fill[EmbeddedListNode[A]](capacity)(new EmbeddedListNode[A])
   private var headIndex = -1
   private var tailIndex = -1
   private var numStored = 0
+  var count = 0
 
   /** Inserts element to tail of list. */
-  def insert(elem: A): Unit = ???
+  def insert(elem: A): Unit = {
+
+    //TODO: null stuff, iterator maybe,
+
+      //if there are no things stored, add it to the list
+
+    //headIndex is where in the array is the first element of the list
+    //listIndex is where in the array is the last element of the list
+
+    if(numStored == 0){
+      dataArray(numStored).value = elem
+      headIndex = 0
+      tailIndex = 0
+    }
+    else{
+
+      if(dataArray(numStored).value == null){
+        dataArray(numStored).value = elem
+      }
+    }
+    numStored += 1
+  }
+
+    for(i <- 0 to capacity - 1){
+      //
+      //      if(dataArray(i).value == null){
+      //        // if empty gonna change the value
+      //        dataArray(i).value = elem
+      //        if(dataArray(i-1) != null){
+      //          dataArray(i).prev = i
+      //        }
+      //      }
+      //
+      //    }
+
+  }
+
 
   /** Removes all copies of the given element. */
-  def remove(elem: A): Boolean = ???
+  def remove(elem: A): Boolean = {
+    //when null the prev and next should always be -1
+    // remember to update numstored
+    return false
+  }
 
   /** Returns the count of nodes containing given entry. */
-  def countEntry(entry: A): Int = ???
+  def countEntry(entry: A): Int = return 0
 
   /** Gets the element at the specified index. */
-  override def apply(idx: Int): A = ???
+  override def apply(idx: Int): A ={ }
 
   /** Replaces element at given index with a new value. */
-  override def update(idx: Int, elem: A): Unit = ???
+  override def update(idx: Int, elem: A): Unit = {}
 
   /** Returns an Iterator that can be used only once. */
   def iterator: Iterator[A] = new Iterator[A] {
